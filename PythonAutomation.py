410d67850a683e2133b7105d6a97ae3303a56dbe
MuhummadShohag <muhummadds@gamil.com>
2021-07-19 16:27:46 +0300

    This is Basic Print, indentation, Speacial Char

---
+++ PythonAutomation.py
@@ -0,0 +1,43 @@
+#Write First Greating
+print("Assalamualikum Muslim People")
+
+''' 
+Indentation:
+    1.indent-> Space
+    2.Indentation are used to represent a block of code 
+    3.The amount of indentation is up to you, but it must be consistent throughout that block
+
+'''
+
+if 1<3:
+    print("Using if Condition")
+    print("We are comparing")
+    print("This is if condition")
+
+print("we are ok with indentation")
+
+# Comments of Python
+'''
+    1. #, If you want to single line comment
+    2.(''' '''), if you want to work with multiple line comment
+
+'''
+
+# This is Single Comment
+'''
+    This is multiple line Comment
+'''
+
+'''
+    Special Characters Of Python
+    1.Write Speacial Chars only inside of quotes--> '', ""
+        \n--> New line
+        \b--> Back Space
+        \t--> New Tab
+        \ --Escape Symbol
+'''
+
+print("Hello \nWorld")
+print("Hello \bWorld")
+print("Hello\tWorld")
+print("C:\\Users\\name\\OneDrive\\Desktop\\DS\\PythonAutomation")
